24b50643e2ac3939ad1078bd400563a976059b0b
242496 Pietrzak Szymon <242496@edu.p.lodz.pl>
2023-03-22 22:44:12 +0100

    BALLS project (I spent whole day doing this)

---
+++ TPW2/Model/Mover.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace TPW2.Model
+{
+    class Mover
+    {
+        static Random r = new Random();
+        Vector2 location;
+        Vector2 velocity;
+        private double width;
+        private double height;
+        private double posX;
+        private double posY;
+        Ellipse ellipse;
+        Brush customColor;
+
+        public Mover(double width, double height, Canvas myCanvas, double posX, double posY)
+        {
+            this.width = width;
+            this.height = height;
+            this.posX = posX;
+            this.posY = posY;
+
+            location = new Vector2((float)posX - 30, (float)posY - 30);
+            velocity = new Vector2(r.Next(-5, 5), r.Next(-5, 5));
+
+            customColor = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255), (byte)r.Next(1, 255), (byte)r.Next(1, 255)));
+            ellipse = new Ellipse()
+            {
+                Tag = "ball1",
+                Width = 50,
+                Height = 50,
+                Fill = customColor,
+                StrokeThickness = 1,
+                Stroke = Brushes.Black
+            };
+            Canvas.SetLeft(ellipse, location.X);
+            Canvas.SetTop(ellipse, location.Y);
+            myCanvas.Children.Add(ellipse);
+        }
+
+        public void Update()
+        {
+            if (location.X < 0 || location.X > width - 65)
+            {
+                velocity.X = velocity.X * -1;
+            }
+            if (location.Y < 0 || location.Y > height - 87)
+            {
+                velocity.Y = velocity.Y * -1;
+            }
+            location = Vector2.Add(location, velocity);
+
+            Canvas.SetLeft(ellipse, location.X);
+            Canvas.SetTop(ellipse, location.Y);
+        }
+    }
+}
---
+++ TPW2/View/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using TPW2.Model;
+
+namespace TPW2
+{
+    public partial class MainWindow : Window
+    {
+        Mover mover;
+        List<Mover> movers = new List<Mover>();
+        int speed = 10;
+        DispatcherTimer timer = new DispatcherTimer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+
+        private void Load()
+        {
+            myCanvas.Focus();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach(var item in movers)
+            {
+                item.Update();
+            }
+        }
+
+        private void AddOrRemoveItems(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is Ellipse)
+            {
+                Ellipse activeElipse = (Ellipse)e.OriginalSource;
+
+                myCanvas.Children.Remove(activeElipse);
+            }
+            else
+            {
+                mover = new Mover(Application.Current.MainWindow.Width, Application.Current.MainWindow.Height, myCanvas, Mouse.GetPosition(myCanvas).X, Mouse.GetPosition(myCanvas).Y);
+                movers.Add(mover);
+            }
+        }
+    }
+}
