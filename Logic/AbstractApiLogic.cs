4e9554affc010419c85457ad5a526bd92d96e6d5
242496 Pietrzak Szymon <242496@edu.p.lodz.pl>
2023-05-15 15:15:27 +0200

    smol changes

--- Logic/AbstractApiLogic.cs
+++ Logic/AbstractApiLogic.cs
@@ -88,11 +88,11 @@ namespace Logic
                     }
                 }
             }
-            if ((ball.LocationX-1 < 0 && ball.VelocityX < 0) || (ball.LocationX-1 > Width - radius * 2 && ball.VelocityX > 0))
+            if ((ball.LocationX < 0 && ball.VelocityX < 0) || (ball.LocationX > Width - radius * 2 && ball.VelocityX > 0))
             {
                 ball.VelocityX = ball.VelocityX * -1;
             }
-            if ((ball.LocationY-1 < 0 && ball.VelocityY < 0) || (ball.LocationY-1 > Height - radius * 2 && ball.VelocityY > 0))
+            if ((ball.LocationY < 0 && ball.VelocityY < 0) || (ball.LocationY > Height - radius * 2 && ball.VelocityY > 0))
             {
                 ball.VelocityY = ball.VelocityY * -1;
             }
